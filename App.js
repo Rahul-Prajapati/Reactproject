c9a1f81b4f6f6715e4bb19a54d060b28818f096b
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2023-09-27 10:50:19 +0530

    day1

--- App.js
+++ App.js
@@ -9,23 +9,53 @@
     </div>
 </div> */}
 
-
-const parent= React.createElement("div",{id:"parent"},
-     [React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"I'm h1 tag"),
-        React.createElement("h2",{},"I'm h2 tag")]
-        ),
-        React.createElement("div",{id:"child2"},
-            [React.createElement("h1",{},"I'm h1 tag"),
-            React.createElement("h2",{},"I'm h2 tag")]
-        )]
-    );
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 
+// const parent= React.createElement("div",{id:"parent"},
+//      [React.createElement("div",{id:"child"},
+//         [React.createElement("h1",{},"Hello World"),
+//         React.createElement("h2",{},"I'm h2 tag")]
+//         ),
+//         React.createElement("div",{id:"child2"},
+//             [React.createElement("h1",{},"I'm h1 tag'"),
+//             React.createElement("h2",{},"I'm h2 tag")]
+//         )]
+//     );
+
+// react element => object => htmlelement(render)
+const parent = React.createElement(
+    "h1",
+    {id:"heading"},
+    "React is again"
+);
+
+// JSX - HTML/XML like syntax
+// JSX - is not HTML in JS
+
+const jsxHeading = <h1 id="heading"> React using JSX </h1>;
 
 console.log(parent);
+console.log(jsxHeading);
 // react object==>html browser understands
 
 // const heading = React.createElement("h1", {id:"heading", xyz:"abc"},"Hello World in React!");
+
+// component composition
+const Title =function () {
+    return (
+    <h1 className='head' tabIndex="5">
+        Namaste React using JSX
+    </h1>
+);
+}
+
+const HeadingComponent =() =>(
+    <div id="container">
+        <Title />
+        <h1 className="heading"> Namaste React Functional Component</h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
